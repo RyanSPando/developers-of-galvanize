5123226f9b70c69367f93d4b4c06a0771631d01b
Ryan "archaen" Pando <vretha@gmail.com>
2016-09-23 14:31:36 -0600

    Working socket

--- src/client/js/play.js
+++ src/client/js/play.js
@@ -2,6 +2,15 @@
 
   'use strict';
 
-  console.log('play');
-
+  // initialize io on the frontend
+  var socket = io();
+  // console.log(socket);
+  $('#chatInput').on('submit', (e) => {
+    e.preventDefault();
+    var msg = $('m').val();
+    console.log(msg);
+    socket.emit('message', msg);
+    // reset form value to nothing
+    $('#m').val('');
+  });
 })();

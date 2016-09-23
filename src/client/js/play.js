6ceb2405794ca9542c042c5363388d6f27f2b4e1
Shane H Pittman <shanehpittman@gmail.com>
2016-09-23 11:26:38 -0600

    connected all pages to js/css

--- src/client/js/play.js
+++ src/client/js/play.js
@@ -2,4 +2,6 @@
 
   'use strict';
 
+  console.log('play');
+
 })();
--- src/server/routes/forgot-password.js
+++ src/server/routes/forgot-password.js
@@ -6,7 +6,7 @@ const indexController = require('../controllers/index');
 router.get('/', function (req, res, next) {
   const renderObject = {};
   renderObject.title = 'Forgot Password!!';
-  res.render('index', renderObject);
+  res.render('./pages/forgot-password', renderObject);
 });
 
 module.exports = router;

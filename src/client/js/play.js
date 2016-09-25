7abf87fc778a5258e770076fece19316836454a3
DickinsonMark <monoposon@comcast.net>
2016-09-25 11:22:31 -0600

    fixed linting errors

--- src/client/js/play.js
+++ src/client/js/play.js
@@ -2,8 +2,8 @@
 //
 //   'use strict';
 //
-//   var canvasElement = document.querySelector("canvas");
-//   var context = canvasElement.getContext("2d");
+//   var canvasElement = document.querySelector('canvas');
+//   var context = canvasElement.getContext('2d');
 //
 //   // the rectangle
 //   context.beginPath();
@@ -16,104 +16,84 @@
 //   context.stroke();
 //
 //   // the fill color
-//   context.fillStyle = "#51DCFF";
+//   context.fillStyle = '#51DCFF';
 //   context.fill();
 //
 // })();
 
-(function(){
-    var canvas = document.getElementById('canvas');
+(function() {
+  var canvas = document.getElementById('canvas');
 
-    var hexHeight,
-        hexRadius,
-        hexRectangleHeight,
-        hexRectangleWidth,
-        hexagonAngle = 0.523598776, // 30 degrees in radians
-        sideLength = 50,
-        boardWidth = 7,
-        boardHeight = 2;
+  var hexHeight = Math.sin(hexagonAngle) * sideLength,
+  hexRadius = Math.cos(hexagonAngle) * sideLength,
+  hexRectangleHeight = sideLength + 2 * hexHeight,
+  hexRectangleWidth = 2 * hexRadius,
+  hexagonAngle = 0.523598776, // 30 degrees in radians
+  sideLength = 50,
+  boardWidth = 7,
+  boardHeight = 2;
 
-    hexHeight = Math.sin(hexagonAngle) * sideLength;
-    hexRadius = Math.cos(hexagonAngle) * sideLength;
-    hexRectangleHeight = sideLength + 2 * hexHeight;
-    hexRectangleWidth = 2 * hexRadius;
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
 
-    if (canvas.getContext){
-        var ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#000000';
+    ctx.strokeStyle = '#CCCCCC';
+    ctx.lineWidth = 1;
 
-        ctx.fillStyle = "#000000";
-        ctx.strokeStyle = "#CCCCCC";
-        ctx.lineWidth = 1;
+    drawBoard(ctx, boardWidth, boardHeight);
 
-        drawBoard(ctx, boardWidth, boardHeight);
+    canvas.addEventListener('mousemove', function(eventInfo) {
+      var x = eventInfo.offsetX || eventInfo.layerX,
+      y = eventInfo.offsetY || eventInfo.layerY,
+      hexY = Math.floor(y / (hexHeight + sideLength)),
+      hexX = Math.floor((x - (hexY % 2) * hexRadius) / hexRectangleWidth),
+      screenX = hexX * hexRectangleWidth + ((hexY % 2) * hexRadius),
+      screenY = hexY * (hexHeight + sideLength);
 
-        canvas.addEventListener("mousemove", function(eventInfo) {
-            var x,
-                y,
-                hexX,
-                hexY,
-                screenX,
-                screenY;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-            x = eventInfo.offsetX || eventInfo.layerX;
-            y = eventInfo.offsetY || eventInfo.layerY;
+      drawBoard(ctx, boardWidth, boardHeight);
 
-
-            hexY = Math.floor(y / (hexHeight + sideLength));
-            hexX = Math.floor((x - (hexY % 2) * hexRadius) / hexRectangleWidth);
-
-            screenX = hexX * hexRectangleWidth + ((hexY % 2) * hexRadius);
-            screenY = hexY * (hexHeight + sideLength);
-
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-            drawBoard(ctx, boardWidth, boardHeight);
-
-            // Check if the mouse's coords are on the board
-            if(hexX >= 0 && hexX < boardWidth) {
-                if(hexY >= 0 && hexY < boardHeight) {
-                    ctx.fillStyle = "#000000";
-                    drawHexagon(ctx, screenX, screenY, true);
-                }
-            }
-        });
-    }
-
-    function drawBoard(canvasContext, width, height) {
-        var i,
-            j;
-
-        for(i = 0; i < height; ++i) {
-          console.log(i, '?height?');
-            for(j = 0; j < width; ++j) {
-              console.log(j, '?width?');
-                drawHexagon(
-                    ctx,
-                    i * hexRectangleWidth + ((j % 2) * hexRadius),
-                    j * (sideLength + hexHeight),
-                    false
-                );
-            }
+      // Check if the mouse's coords are on the board
+      if (hexX >= 0 && hexX < boardWidth) {
+        if (hexY >= 0 && hexY < boardHeight) {
+          ctx.fillStyle = '#000000';
+          drawHexagon(ctx, screenX, screenY, true);
         }
+      }
+    });
+  }
+
+  function drawBoard(canvasContext, width, height) {
+    var i,
+    j;
+
+    for (i = 0; i < height; ++i) {
+      console.log(i, '?height?');
+      for (j = 0; j < width; ++j) {
+        console.log(j, '?width?');
+        drawHexagon(
+          ctx,
+          i * hexRectangleWidth + ((j % 2) * hexRadius),
+          j * (sideLength + hexHeight),
+          false
+        );
+      }
     }
+  }
 
-    function drawHexagon(canvasContext, x, y, fill) {
-        var fill = fill || false;
+  function drawHexagon(canvasContext, x, y, fill = false) {
+    canvasContext.beginPath();
+    canvasContext.moveTo(x + hexRadius, y);
+    canvasContext.lineTo(x + hexRectangleWidth, y + hexHeight);
+    canvasContext.lineTo(x + hexRectangleWidth, y + hexHeight + sideLength);
+    canvasContext.lineTo(x + hexRadius, y + hexRectangleHeight);
+    canvasContext.lineTo(x, y + sideLength + hexHeight);
+    canvasContext.lineTo(x, y + hexHeight);
+    canvasContext.closePath();
 
-        canvasContext.beginPath();
-        canvasContext.moveTo(x + hexRadius, y);
-        canvasContext.lineTo(x + hexRectangleWidth, y + hexHeight);
-        canvasContext.lineTo(x + hexRectangleWidth, y + hexHeight + sideLength);
-        canvasContext.lineTo(x + hexRadius, y + hexRectangleHeight);
-        canvasContext.lineTo(x, y + sideLength + hexHeight);
-        canvasContext.lineTo(x, y + hexHeight);
-        canvasContext.closePath();
-
-        if(fill) {
-            canvasContext.fill();
-        } else {
-            canvasContext.stroke();
-        }
-    }
+    if (fill) canvasContext.fill();
+    else canvasContext.stroke();
+  }
 
 })();

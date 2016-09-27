96884a50cbe091416c674659eab4e6d12f360bf3
Ryan "archaen" Pando <vretha@gmail.com>
2016-09-27 10:24:45 -0600

    Fix chat.

--- src/server/routes/play.js
+++ src/server/routes/play.js
@@ -4,7 +4,7 @@ const authHelpers = require('../controllers/signin');
 
 const indexController = require('../controllers/index');
 
-router.get('/', (req, res, next) => {
+router.get('/', authHelpers.loginRequired, (req, res, next) => {
   const renderObject = {};
   renderObject.title = 'Play!';
   renderObject.sessionID = req.sessionID;

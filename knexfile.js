76d59ba0700cf8a9aaa869a2a35cc8bdee8088f7
Ho0dLuM <Jordon.Hoshor@gmail.com>
2016-09-29 12:26:45 -0600

    changed knexfile for heroku

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'ec2-54-163-230-103.compute-1.amazonaws.com'
+      database: 'postgres://vroppzvgsspsem:knp9mzkAL3N5fN5qAgdj01naW_@ec2-54-163-230-103.compute-1.amazonaws.com:5432/d7a5jse1ic8vo0'
     },
     migrations: {
       directory: __dirname + '/src/server/db/migrations'
